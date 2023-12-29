d2d403529f43933f24b33f366597eef06e9a9320
dm <heydruavh@gmail.com>
2023-12-30 00:56:55 +0530

    TTT logic built

--- 5_TicTacToe/script.js
+++ 5_TicTacToe/script.js
@@ -1,37 +1,44 @@
-const boxes = document.querySelectorAll(".box")
-let player1 = true
+const boxes = document.querySelectorAll(".box");
+let player1 = true;
+const winnerPatterns = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 
-const winnerPaterns = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6]
-]
-    console.log(boxes)
-    console.log(winnerPaterns)
-
-
-const checkWinner = ()=>{
-    for (let pattern in winnerPaterns) {
-        console.log(pattern[0])
+boxes.forEach((box) => {
+  //for each box in the boxes container
+  box.addEventListener("click", () => {
+    // Added event listeners on each box
+    //Player 1 by default
+    if (player1) {
+      box.textContent = "O";
+      player1 = false;
+    } else {
+      box.textContent = "X";
+      player1 = true;
     }
-}
+    //disables each box to prevent overwrite
+    box.disabled = true;
 
+    checkWinner();
+  });
+});
 
-boxes.forEach((box)=>{                           //loops on each box
-    box.addEventListener("click", ()=>{
-        if(player1){                             //player1 === true
-            box.textContent = "O"
-            player1 = false
-        } else{
-            box.textContent = "X"
-            player1 = true
+const checkWinner = () => {
+  for (let pattern of winnerPatterns) {
+    let pos1 = boxes[pattern[0]].textContent;
+    let pos2 = boxes[pattern[1]].textContent;
+    let pos3 = boxes[pattern[2]].textContent;
+    if(pos1 != "" && pos2 != "" && pos3 != ""){
+        if(pos1 === pos2 && pos2 === pos3){
+            alert("Game Finished")
         }
-        box = box.ariaDisabled                   // disables the box after used once
-        checkWinner()
-    })
-})
+    }
+  }
+};
