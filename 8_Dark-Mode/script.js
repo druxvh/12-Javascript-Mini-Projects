a3f48dd8a58b41c4257f957b01511a93ef2efe4c
dm <heydruavh@gmail.com>
2024-01-06 00:20:09 +0530

    Added dark mode func & logic

--- 8_Dark-Mode/script.js
+++ 8_Dark-Mode/script.js
@@ -1,1 +1,47 @@
-console.log("test okay")
+const mode = document.querySelector("button")
+const article = document.querySelector("article")
+const paragraphs = document.querySelectorAll("p")
+const heading = document.querySelector("h1")
+const aside = document.querySelector("aside")
+let defaultMode;
+console.log(mode)
+console.log(article)
+console.log(paragraphs)
+console.log(heading)
+
+let darkmode = () => {
+    mode.textContent = "Dark Mode"
+    mode.style.backgroundColor = "#f1f1f1"
+    mode.style.color = "#111111"
+    paragraphs.forEach((para)=>{
+        para.style.color = "#f1f1f1"
+    })
+    heading.style.color = "#f1f1f1"
+    document.body.style.backgroundColor = "#191919"
+    article.style.backgroundColor = "#202020"
+    aside.style.backgroundColor = "#202020"
+    defaultMode = false
+} 
+let lightmode = () => {
+    mode.textContent = "Light Mode"
+    mode.style.backgroundColor = "#111111"
+    mode.style.color = "#f1f1f1"
+    paragraphs.forEach((para)=>{
+        para.style.color = "#111111"
+    })
+    heading.style.color = "#111111"
+    document.body.style.backgroundColor = "#fff"
+    article.style.backgroundColor = "#dddddd"
+    aside.style.backgroundColor = "#fbfea5"
+    defaultMode = true
+}
+lightmode()
+
+mode.addEventListener("click", ()=>{
+    if(defaultMode){
+        darkmode()
+        defaultMode = false
+    } else{
+        lightmode()
+    }
+})
