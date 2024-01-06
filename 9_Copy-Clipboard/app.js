b3d0fd4d8da78c6597575cde381ba5a14a0126a3
dm <heydruavh@gmail.com>
2024-01-07 00:11:54 +0530

    Added copy function

--- 9_Copy-Clipboard/app.js
+++ 9_Copy-Clipboard/app.js
@@ -1,1 +1,7 @@
-console.log("test ok");
+const selectText = document.querySelector("textarea");
+const copyBtn = document.querySelector("button");
+let str = "";
+copyBtn.addEventListener("click", () => {
+  selectText.select(); // highlights the text
+  navigator.clipboard.writeText(selectText.value); // clips the text to clipboard
+});
