c01c3e61a343f23855da5cd6890de5979d48f1fe
dm <heydruavh@gmail.com>
2024-01-17 23:17:38 +0530

    Updated Logic

--- 11_Stopwatch/script.js
+++ 11_Stopwatch/script.js
@@ -1,41 +1,40 @@
-const displayTime = document.querySelector(".stop-watch");
+const time = document.querySelector(".stopwatch");
 const startBtn = document.querySelector(".start-el");
 const pauseBtn = document.querySelector(".pause-el");
 const resetBtn = document.querySelector(".reset-el");
 
-let minutes = 0;
-let seconds = 0;
-let timer;
+let isRunning = false;
+let [hours, mins, secs] = [0, 0, 0];
 
 startBtn.addEventListener("click", () => {
-  timer = true;
-  stopwatch();
+  isRunning = true;
+  setInterval(stopwatch, 1000);
 });
 pauseBtn.addEventListener("click", () => {
-  timer = false;
+  isRunning = false;
 });
 resetBtn.addEventListener("click", () => {
-  timer = false;
-  minutes = 0;
-  seconds = 0;
-  displayTime.textContent = `00:00`;
+  isRunning = false;
+  [hours, mins, secs] = [0, 0, 0];
+  console.log(hours, mins, secs);
 });
 
 const stopwatch = () => {
-  if (timer) {
-    seconds++;
-    if (seconds == 60) {
-      minutes++;
-      seconds = 0;
-    }
-    if (minutes == 60) {
-      timer = false;
-    }
+  if (isRunning) {
+    secs++;
 
-    seconds = seconds < 10 ? "0" + seconds : seconds;
-    minutes = minutes < 10 ? "0" + minutes : minutes;
-
-    displayTime.textContent = `${minutes}:${seconds}`;
-    setTimeout(stopwatch, 1000);
+    if (secs == 60) {
+      mins++;
+      secs = 0;
+    }
+    if (mins == 60) {
+      hours++;
+      mins = 0;
+      secs = 0;
+    }
   }
+  let h = hours < 10 ? "0" + hours : hours;
+  let m = mins < 10 ? "0" + mins : mins;
+  let s = secs < 10 ? "0" + secs : secs;
+  time.textContent = `${h}:${m}:${s}`;
 };
